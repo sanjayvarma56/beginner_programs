947ec7a161901ba04379647ee1f0c61a33a04c20
sanjayvarma56 <dssvarma56@gmail.com>
2026-06-18 08:34:08 +0530

    program4(swap two numbers)

---
+++ program4.py
@@ -0,0 +1,16 @@
+#To swap two numbers
+a = 4
+b = 8
+print("Before swapping: a =", a, "and b =", b)
+temp = a
+a = b
+b = temp
+print("After swapping: a =", a, "and b =", b)
+
+#OR
+
+a = 4
+b = 8
+print("Before swapping: a =", a, "and b =", b)
+a, b = b, a
+print("After swapping: a =", a, "and b =", b)
