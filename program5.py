567c976e9a8152ee8ba044e156e3cd063b95d507
sanjayvarma56 <dssvarma56@gmail.com>
2026-06-18 08:35:36 +0530

    program5(sqaure and cube of a number)

---
+++ program5.py
@@ -0,0 +1,9 @@
+#To find square and cube of a number
+a = int(input("Enter a number:"))
+print("The number you entered is", a)
+print("The square of the number is", a*a)
+#OR 
+print("The square of the number is ",a**2)
+print("The cube of the number is ", a*a*a)
+#OR
+print("The cube of the number is ", a**3)
