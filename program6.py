08e6e8a1f53322544b9fba975e70afecd016193a
sanjayvarma56 <dssvarma56@gmail.com>
2026-06-19 08:06:53 +0530

    program6(Area of Rectangle)

---
+++ program6.py
@@ -0,0 +1,5 @@
+#Area of Rectangle
+length = int(input("Enter length of the rectangle"))
+breadth = int(input("Enter breadth of the rectangle"))
+area = length * breadth
+print("Area of rectangle: ",area)
