c8b7c24c1fb01183b7db775f24f0c349d1314554
sanjayvarma56 <dssvarma56@gmail.com>
2026-07-25 22:24:42 +0530

    program8(Celcius to Farenheit)

---
+++ program8.py
@@ -0,0 +1,5 @@
+celcius = float(input("Enter the temperature in celcius: "))
+farenheit = (celcius * 9/5) + 32
+print("Temperature in farenheit:",farenheit)
+#OR
+print(f"{celcius}°C = {farenheit}°F")
